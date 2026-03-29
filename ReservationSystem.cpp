9e91325baf50bc54ae604fe43e90543f8a8b8197
Trajano <pedroetrajano@gmail.com>
2026-03-29 10:53:24 -0300

    feat: implement cancel and printSchedule methods

--- ReservationSystem.cpp
+++ ReservationSystem.cpp
@@ -27,24 +27,28 @@ ReservationSystem::~ReservationSystem() {
 }
 
 bool ReservationSystem::reserve(ReservationRequest request) {
-    int interval = request.getEndHour() - request.getStartHour();
+    int start_hour = request.getStartHour();
+    int end_hour = request.getEndHour();
+    int interval = end_hour - start_hour;
+    int student_count = request.getStudentCount();
     int day_index = -1;
     bool result = false;
+    std::string week_day = request.getWeekday();
     std::string week[] = {"segunda", "terça", "quarta", "quinta", "sexta"};
     ReservationRequest* req = new ReservationRequest(request);
 
     for (int day = 0; day < 5; day++) {
-        if (week[day] == request.getWeekday()) {
+        if (week[day] == week_day) {
             day_index = day;
         }
     }
 
     for (int room = 0; room < room_count; room++) {
-        if (room_capacities[room] >= request.getStudentCount()) {
+        if (room_capacities[room] >= student_count) {
             result = true;
 
             for (int hour = 0; hour < interval; hour++) {
-                if (room_schedule[room][day_index][request.getStartHour() + hour - 7] != nullptr) {
+                if (room_schedule[room][day_index][start_hour + hour - 7] != nullptr) {
                     result = false;
                     break;
                 }
@@ -52,7 +56,7 @@ bool ReservationSystem::reserve(ReservationRequest request) {
 
             if (result) {
                 for (int hour = 0; hour < interval; hour++) {
-                    room_schedule[room][day_index][request.getStartHour() + hour - 7] = req;
+                    room_schedule[room][day_index][start_hour + hour - 7] = req;
                 }
                 
                 return result;
@@ -64,9 +68,71 @@ bool ReservationSystem::reserve(ReservationRequest request) {
     return result;
 }
 bool ReservationSystem::cancel(std::string course_name) {
-    
+    bool result = false;
+
+    for (int room = 0; room < room_count; room++) {
+        for (int day = 0; day < 5; day++) {
+            for (int hour = 0; hour < 14; hour++) {
+                ReservationRequest* req = room_schedule[room][day][hour];
+
+                if (req != nullptr && req->getCourseName() == course_name) {
+                    delete req;
+                    req = nullptr;
+
+                    result = true;
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
 void ReservationSystem::printSchedule() {
-    
+    for (int room = 0; room < room_count; room++) {
+        bool check = false;
+
+        for (int day = 0; day < 5; day++) {
+            for (int hour = 0; hour < 14; hour++) {
+                ReservationRequest* req = room_schedule[room][day][hour];
+                if (req != nullptr) {
+                    check = true;
+                    break;
+                }
+            }
+        }
+
+        if (check) {
+            std::cout << "Room " << room + 1 << std::endl;
+        }
+        
+        for (int day = 0; day < 5; day++) {
+            check = false;
+            std::string week_day = "";
+        
+            for (int hour = 0; hour < 14; hour++) {
+                ReservationRequest* req = room_schedule[room][day][hour];
+                if (req != nullptr) {
+                    check = true;
+                    week_day = req->getWeekday();
+                    break;
+                }
+            }
+
+            if (check) {
+                std::cout << week_day << ":" << std::endl;
+            }
+            
+            for (int hour = 0; hour < 14; hour++) {
+                ReservationRequest* pre_req = room_schedule[room][day][hour - 1];
+                ReservationRequest* req = room_schedule[room][day][hour];
+                if (req != nullptr && (hour == 0 || req != pre_req)) {
+                    int start_hour = req->getStartHour();
+                    int end_hour = req->getEndHour();
+                    std::string course_name = req->getCourseName();
+                    std::cout << start_hour << "h~" << end_hour << "h: " << course_name << std::endl;
+                }
+            }
+        }
+    }    
 }
